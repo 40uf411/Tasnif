c5dd073e0e0d890712fc0970474fac6e3c420ba9
ali <ali25ab@outlook.com>
2019-08-14 23:08:52 +0100

    first upload: main theme set up

---
+++ js/main.js
@@ -0,0 +1,266 @@
+$(document).ready(function () {
+    "use strict";
+    var window_width = $(window).width(),
+        window_height = window.innerHeight,
+        header_height = $(".default-header").height(),
+        header_height_static = $(".site-header.static").outerHeight(),
+        fitscreen = window_height - header_height;
+    $(".fullscreen").css("height", window_height)
+    $(".fitscreen").css("height", fitscreen);
+    new WOW().init();
+    if (document.getElementById("default-select")) {
+        $('select').niceSelect();
+    };
+    $('.img-pop-up').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+    $('.play-btn').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+    $('#back-top a').on("click", function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    $('.nav-menu').superfish({
+        animation: {
+            opacity: 'show'
+        },
+        speed: 400
+    });
+    if ($('#nav-menu-container').length) {
+        var $mobile_nav = $('#nav-menu-container').clone().prop({
+            id: 'mobile-nav'
+        });
+        $mobile_nav.find('> ul').attr({
+            'class': '',
+            'id': ''
+        });
+        $('body').append($mobile_nav);
+        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
+        $('body').append('<div id="mobile-body-overly"></div>');
+        $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
+        $(document).on('click', '.menu-has-children i', function (e) {
+            $(this).next().toggleClass('menu-item-active');
+            $(this).nextAll('ul').eq(0).slideToggle();
+            $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
+        });
+        $(document).on('click', '#mobile-nav-toggle', function (e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+            $('#mobile-body-overly').toggle();
+        });
+        $(document).click(function (e) {
+            var container = $("#mobile-nav, #mobile-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
+                    $('#mobile-body-overly').fadeOut();
+                }
+            }
+        });
+    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+        $("#mobile-nav, #mobile-nav-toggle").hide();
+    }
+    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
+                var top_space = 0;
+                if ($('#header').length) {
+                    top_space = $('#header').outerHeight();
+                    if (!$('#header').hasClass('header-fixed')) {
+                        top_space = top_space;
+                    }
+                }
+                $('html, body').animate({
+                    scrollTop: target.offset().top - top_space
+                }, 1500, 'easeInOutExpo');
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('#mobile-nav-toggle i').toggleClass('lnr-times lnr-bars');
+                    $('#mobile-body-overly').fadeOut();
+                }
+                return false;
+            }
+        }
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+            $('#back-top').fadeIn(500);
+        } else {
+            $('#header').removeClass('header-scrolled');
+            $('#back-top').fadeOut(500);
+        }
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('#back-top').fadeIn(500);
+        } else {
+            $('#back-top').fadeOut(500);
+        }
+    });
+    if ($('.testi-slider').length) {
+        $('.testi-slider').owlCarousel({
+            loop: true,
+            margin: 30,
+            items: 1,
+            nav: false,
+            autoplay: 2500,
+            smartSpeed: 1500,
+            dots: true,
+            responsiveClass: true,
+            thumbs: true,
+            thumbsPrerendered: true,
+            navText: ["<i class='lnr lnr-arrow-left'></i>", "<i class='lnr lnr-arrow-right'></i>"]
+        })
+    }
+    if (document.getElementById("map")) {
+        google.maps.event.addDomListener(window, 'load', init);
+
+        function init() {
+            var mapOptions = {
+                zoom: 11,
+                center: new google.maps.LatLng(40.6700, -73.9400),
+                styles: [{
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#e9e9e9"
+                    }, {
+                        "lightness": 17
+                    }]
+                }, {
+                    "featureType": "landscape",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#f5f5f5"
+                    }, {
+                        "lightness": 20
+                    }]
+                }, {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                        "color": "#ffffff"
+                    }, {
+                        "lightness": 17
+                    }]
+                }, {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": [{
+                        "color": "#ffffff"
+                    }, {
+                        "lightness": 29
+                    }, {
+                        "weight": 0.2
+                    }]
+                }, {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#ffffff"
+                    }, {
+                        "lightness": 18
+                    }]
+                }, {
+                    "featureType": "road.local",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#ffffff"
+                    }, {
+                        "lightness": 16
+                    }]
+                }, {
+                    "featureType": "poi",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#f5f5f5"
+                    }, {
+                        "lightness": 21
+                    }]
+                }, {
+                    "featureType": "poi.park",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#dedede"
+                    }, {
+                        "lightness": 21
+                    }]
+                }, {
+                    "elementType": "labels.text.stroke",
+                    "stylers": [{
+                        "visibility": "on"
+                    }, {
+                        "color": "#ffffff"
+                    }, {
+                        "lightness": 16
+                    }]
+                }, {
+                    "elementType": "labels.text.fill",
+                    "stylers": [{
+                        "saturation": 36
+                    }, {
+                        "color": "#333333"
+                    }, {
+                        "lightness": 40
+                    }]
+                }, {
+                    "elementType": "labels.icon",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                }, {
+                    "featureType": "transit",
+                    "elementType": "geometry",
+                    "stylers": [{
+                        "color": "#f2f2f2"
+                    }, {
+                        "lightness": 19
+                    }]
+                }, {
+                    "featureType": "administrative",
+                    "elementType": "geometry.fill",
+                    "stylers": [{
+                        "color": "#fefefe"
+                    }, {
+                        "lightness": 20
+                    }]
+                }, {
+                    "featureType": "administrative",
+                    "elementType": "geometry.stroke",
+                    "stylers": [{
+                        "color": "#fefefe"
+                    }, {
+                        "lightness": 17
+                    }, {
+                        "weight": 1.2
+                    }]
+                }]
+            };
+            var mapElement = document.getElementById('map');
+            var map = new google.maps.Map(mapElement, mapOptions);
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(40.6700, -73.9400),
+                map: map,
+                title: 'Snazzy!'
+            });
+        }
+    }
+    $('#mc_embed_signup').find('form').ajaxChimp();
+});
---
+++ query.php
@@ -0,0 +1,225 @@
+ <!DOCTYPE html>
+ <html lang="eng" class="no-js">
+
+ <head>
+
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <link rel="shortcut icon" href="img/fav.png">
+
+     <meta name="author" content="codepixer">
+
+     <meta name="description" content="">
+
+     <meta name="keywords" content="">
+
+     <meta charset="UTF-8">
+
+     <title>Educature Education</title>
+     <!--<link href="https://fonts.googleapis.com/css?family=:300,500,600" rel="stylesheet">-->
+     <link rel="stylesheet" href="css/Montserrat.css">
+     <!--<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500i" rel="stylesheet">-->
+     <link rel="stylesheet" href="css/Roboto.css">
+
+     <link rel="stylesheet" href="css/themify-icons.css">
+     <link rel="stylesheet" href="css/linearicons.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/bootstrap.css">
+     <link rel="stylesheet" href="css/magnific-popup.css">
+     <link rel="stylesheet" href="css/nice-select.css">
+     <link rel="stylesheet" href="css/animate.min.css">
+     <link rel="stylesheet" href="css/owl.carousel.css">
+     <link rel="stylesheet" href="css/main.css">
+     <link rel="stylesheet" href="css/style.css">
+ </head>
+
+ <body>
+
+     <header id="header">
+         <div class="container">
+             <div class="row align-items-center justify-content-between d-flex">
+                 <div id="logo">
+                     <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+                 </div>
+                 <nav id="nav-menu-container">
+                     <ul class="nav-menu">
+                         <li class="menu-active"><a href="index.html">Home</a></li>
+                         <li><a href="about.html">About</a></li>
+                         <li><a href="courses.html">Courses</a></li>
+                         <li class="menu-has-children"><a href="index.html">Pages</a>
+                             <ul>
+                                 <li><a href="elements.html">Elements</a></li>
+                             </ul>
+                         </li>
+                         <li class="menu-has-children"><a href="index.html">Blog</a>
+                             <ul>
+                                 <li><a href="blog-home.html">Blog Home</a></li>
+                                 <li><a href="blog-single.html">Blog Details</a></li>
+                             </ul>
+                         </li>
+                         <li><a href="contact.html">Contact</a></li>
+                     </ul>
+                 </nav>
+             </div>
+         </div>
+     </header>
+
+     <section class="search-header-area relative">
+         <div class="top-part">
+             <div class="big">
+                 <h1 class="title">Tasnif</h1>
+             </div>
+         </div>
+         <div class="input-part">
+             <div class="form-group">
+                 <input type="text" class="form-control" id="search-input" placeholder="Search for a magazine..">
+             </div>
+         </div>
+     </section>
+
+     <section class="search-results-area relative">
+         <div class="container">
+             <div class="card mb-3">
+                 <div class="card-body">
+                     <h5 class="card-title">Cahiers?lisab?thains: A Journal of English Renaissance Studies</h5>
+                     <p class="card-text">
+                         <p class="class">CLASS :(B)</p>
+                         <p class="pub">Publisher:Sage Publications | ISSN: 0184-7678, ESSN : 2054-4715</p>
+                     </p>
+                     <a href="https://www.scimagojr.com/journalsearch.php?q=Cahiers+?lisab?thains:+A+Journal+of+English+Renaissance+Studies"
+                         class="card-link">Magazine url</a>
+                 </div>
+             </div>
+             <div class="card mb-3">
+                 <div class="card-body">
+                     <h5 class="card-title">Cahiers?lisab?thains: A Journal of English Renaissance Studies</h5>
+                     <p class="card-text">
+                         <p class="class">CLASS :(B)</p>
+                         <p class="pub">Publisher:Sage Publications | ISSN: 0184-7678, ESSN : 2054-4715</p>
+                     </p>
+                     <a href="https://www.scimagojr.com/journalsearch.php?q=Cahiers+?lisab?thains:+A+Journal+of+English+Renaissance+Studies"
+                         class="card-link">Magazine url</a>
+                 </div>
+             </div>
+             <div class="card mb-3">
+                 <div class="card-body">
+                     <h5 class="card-title">Cahiers?lisab?thains: A Journal of English Renaissance Studies</h5>
+                     <p class="card-text">
+                         <p class="class">CLASS :(B)</p>
+                         <p class="pub">Publisher:Sage Publications | ISSN: 0184-7678, ESSN : 2054-4715</p>
+                     </p>
+                     <a href="https://www.scimagojr.com/journalsearch.php?q=Cahiers+?lisab?thains:+A+Journal+of+English+Renaissance+Studies"
+                         class="card-link">Magazine url</a>
+                 </div>
+             </div>
+         </div>
+     </section>
+
+     <footer class="footer-area section-gap">
+         <div class="container">
+             <div class="row">
+                 <div class="col-lg-2 col-md-6 single-footer-widget">
+                     <h4>Top Products</h4>
+                     <ul>
+                         <li><a href="index.html#">Managed Website</a></li>
+                         <li><a href="index.html#">Manage Reputation</a></li>
+                         <li><a href="index.html#">Power Tools</a></li>
+                         <li><a href="index.html#">Marketing Service</a></li>
+                     </ul>
+                 </div>
+                 <div class="col-lg-2 col-md-6 single-footer-widget">
+                     <h4>Quick Links</h4>
+                     <ul>
+                         <li><a href="index.html#">Jobs</a></li>
+                         <li><a href="index.html#">Brand Assets</a></li>
+                         <li><a href="index.html#">Investor Relations</a></li>
+                         <li><a href="index.html#">Terms of Service</a></li>
+                     </ul>
+                 </div>
+                 <div class="col-lg-2 col-md-6 single-footer-widget">
+                     <h4>Features</h4>
+                     <ul>
+                         <li><a href="index.html#">Jobs</a></li>
+                         <li><a href="index.html#">Brand Assets</a></li>
+                         <li><a href="index.html#">Investor Relations</a></li>
+                         <li><a href="index.html#">Terms of Service</a></li>
+                     </ul>
+                 </div>
+                 <div class="col-lg-2 col-md-6 single-footer-widget">
+                     <h4>Resources</h4>
+                     <ul>
+                         <li><a href="index.html#">Guides</a></li>
+                         <li><a href="index.html#">Research</a></li>
+                         <li><a href="index.html#">Experts</a></li>
+                         <li><a href="index.html#">Agencies</a></li>
+                     </ul>
+                 </div>
+                 <div class="col-lg-4 col-md-6 single-footer-widget">
+                     <h4>Newsletter</h4>
+                     <p>You can trust us. we only send promo offers,</p>
+                     <div class="form-wrap" id="mc_embed_signup">
+                         <form target="_blank"
+                             action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                             method="get" class="form-inline">
+                             <input class="form-control" name="EMAIL" placeholder="Your Email Address"
+                                 onfocus="if (!window.__cfRLUnblockHandlers) return false; this.placeholder = ''"
+                                 onblur="if (!window.__cfRLUnblockHandlers) return false; this.placeholder = 'Your Email Address '"
+                                 required="" type="email" data-cf-modified-fe4646238a4462a1619516de-="">
+                             <button class="click-btn btn btn-default"><span
+                                     class="lnr lnr-arrow-right"></span></button>
+                             <div style="position: absolute; left: -5000px;">
+                                 <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value=""
+                                     type="text">
+                             </div>
+                             <div class="info"></div>
+                         </form>
+                     </div>
+                 </div>
+             </div>
+             <div class="footer-bottom row align-items-center">
+                 <p class="footer-text m-0 col-lg-8 col-md-12">
+                     Copyright &copy;<script type="fe4646238a4462a1619516de-text/javascript">
+                         document.write(new Date().getFullYear());
+                     </script> All rights reserved | This template is made with <i class="fa fa-heart-o"
+                         aria-hidden="true"></i> by
+                     <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                 </p>
+                 <div class="col-lg-4 col-md-12 footer-social">
+                     <a href="index.html#"><i class="fa fa-facebook"></i></a>
+                     <a href="index.html#"><i class="fa fa-twitter"></i></a>
+                     <a href="index.html#"><i class="fa fa-dribbble"></i></a>
+                     <a href="index.html#"><i class="fa fa-behance"></i></a>
+                 </div>
+             </div>
+         </div>
+     </footer>
+
+
+     <div id="back-top">
+         <a title="Go to Top" href="index.html#"></a>
+     </div>
+
+     <script src="js/vendor/jquery-2.2.4.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/popper.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/vendor/bootstrap.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/easing.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/hoverIntent.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/superfish.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/jquery.ajaxchimp.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/jquery.magnific-popup.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/owl.carousel.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/owl-carousel-thumb.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/jquery.sticky.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/jquery.nice-select.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/parallax.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/waypoints.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/wow.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/jquery.counterup.min.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/mail-script.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+     <script src="js/main.js" type="fe4646238a4462a1619516de-text/javascript"></script>
+
+
+     <script src="js/rocket-loader.min.js" data-cf-settings="fe4646238a4462a1619516de-|49" defer=""></script>
+ </body>
+
+ </html>
