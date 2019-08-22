392176ec093ebc5f0426c16db5e36bb7cd4f73fe
Smail Abd El Madjid <40744470+Abdelmadjidsmail@users.noreply.github.com>
2019-08-22 13:52:32 +0100

    Add files via upload

---
+++ conf/config.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "madjidsmail";
+$database="laliste";
+
+
+
+
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+
+
+
+?>
